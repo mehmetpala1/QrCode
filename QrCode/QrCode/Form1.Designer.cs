32d9cab2977ad65929f68f5e8c3630107b1af8cb
Mehmet Pala <mhmtpl@gmail.com>
2015-08-29 12:41:28 +0300

    Basic QrCode Creator
    QrCode Creator

---
+++ QrCode/QrCode/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace QrCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7"),
+            resources.GetString("comboBox1.Items8"),
+            resources.GetString("comboBox1.Items9"),
+            resources.GetString("comboBox1.Items10"),
+            resources.GetString("comboBox1.Items11"),
+            resources.GetString("comboBox1.Items12"),
+            resources.GetString("comboBox1.Items13"),
+            resources.GetString("comboBox1.Items14"),
+            resources.GetString("comboBox1.Items15"),
+            resources.GetString("comboBox1.Items16"),
+            resources.GetString("comboBox1.Items17"),
+            resources.GetString("comboBox1.Items18"),
+            resources.GetString("comboBox1.Items19"),
+            resources.GetString("comboBox1.Items20"),
+            resources.GetString("comboBox1.Items21"),
+            resources.GetString("comboBox1.Items22"),
+            resources.GetString("comboBox1.Items23"),
+            resources.GetString("comboBox1.Items24"),
+            resources.GetString("comboBox1.Items25"),
+            resources.GetString("comboBox1.Items26"),
+            resources.GetString("comboBox1.Items27"),
+            resources.GetString("comboBox1.Items28"),
+            resources.GetString("comboBox1.Items29"),
+            resources.GetString("comboBox1.Items30"),
+            resources.GetString("comboBox1.Items31"),
+            resources.GetString("comboBox1.Items32"),
+            resources.GetString("comboBox1.Items33"),
+            resources.GetString("comboBox1.Items34"),
+            resources.GetString("comboBox1.Items35"),
+            resources.GetString("comboBox1.Items36"),
+            resources.GetString("comboBox1.Items37"),
+            resources.GetString("comboBox1.Items38"),
+            resources.GetString("comboBox1.Items39")});
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2"),
+            resources.GetString("comboBox2.Items3")});
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.Name = "comboBox2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+           // this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ QrCode/QrCode/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using MessagingToolkit.QRCode;
+using MessagingToolkit.QRCode.Codec;
+
+namespace QrCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Image QRCodeOlustur(string Veri1, int QrCodeDuzey)
+        {
+            
+                string veri = Veri1;
+                MessagingToolkit.QRCode.Codec.QRCodeEncoder QrCodeEncode = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+                QrCodeEncode.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+                string ECC = comboBox2.SelectedItem.ToString();
+                if (ECC == "L") { QrCodeEncode.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.L; }
+                else if (ECC == "M") { QrCodeEncode.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.M; }
+                else if (ECC == "Q") { QrCodeEncode.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.Q; }
+                else if (ECC == "H") { QrCodeEncode.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.H; }
+               
+                  QrCodeEncode.QRCodeVersion = QrCodeDuzey;
+                  try
+                  {
+                      System.Drawing.Bitmap BitMap = QrCodeEncode.Encode(veri);
+                      return BitMap; 
+                  }
+                  catch (Exception)
+                  {   MessageBox.Show("Lütfen Kategori Ayarını Doğru Seçiniz");
+                      string hata="Hata";
+                      System.Drawing.Bitmap BitMap2 = QrCodeEncode.Encode(hata);
+                      return BitMap2;
+                  }
+                
+               
+         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image ResimAl;
+            ResimAl = pictureBox1.Image;
+            ResimAl.Save("D:\\" + RandomString(4) + ".jpg");
+            MessageBox.Show("Olusturulan QR Code’unuz D’ye Kaydedildi.");
+        }
+
+        private string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int seviye = Convert.ToInt32(comboBox1.SelectedItem.ToString());
+                pictureBox1.Image = QRCodeOlustur(textBox1.Text, seviye);
+            }
+            catch (Exception) {
+                MessageBox.Show("Lütfen Kategori Ayarlarını Seçiniz");
+            }
+        }
+
+        
+    }
+}
